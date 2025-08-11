d293c622cd0b2c8aa15753713d21fd6bbeb9e67b
shifa-imran-ali <shifaimran523@gmail.com>
2025-08-11 20:08:01 +0500

    push

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://heckathon-frontend-tan.vercel.app/",
   credentials: true
 }));
 
